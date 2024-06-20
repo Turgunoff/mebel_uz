763eb4d2de48ad3d172a7d6318de93484ff55f87
Eldor <eldorturgunov777@gmail.com>
2024-06-20 12:30:15 +0500

    new

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
@@ -15,28 +14,10 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-
   @override
   void initState() {
     super.initState();
-    _signInAnonymously(); // Anonim autentifikatsiyani boshlash
-    // Timer(const Duration(seconds: 3), () => Get.offAll(() => NavigationMenu()));
-  }
-
-  Future<void> _signInAnonymously() async {
-    try {
-      await _auth.signInAnonymously();
-
-      // Autentifikatsiyadan so'ng, NavigationMenu ga o'tish
-      Timer(
-        const Duration(seconds: 3),
-        () => Get.offAll(() => NavigationMenu()),
-      );
-    } catch (e) {
-      // Xatolikni qayta ishlash (loglash, snackbar yoki dialog ko'rsatish)
-      print('Error signing in anonymously: $e');
-    }
+    Timer(const Duration(seconds: 3), () => Get.offAll(() => NavigationMenu()));
   }
 
   @override
