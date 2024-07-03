7ea76de8b75aca01af5ebfb56456110e0f833cc0
Eldor <eldorturgunov777@gmail.com>
2024-07-03 16:47:22 +0500

    new

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -17,7 +17,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(const Duration(seconds: 3), () => Get.offAll(() => NavigationMenu()));
+    Timer(const Duration(seconds: 2), () => Get.offAll(() => NavigationMenu()));
   }
 
   @override
