18e5aff67c58708ad64175845339e1bcb3c015d6
Eldor <eldorturgunov777@gmail.com>
2024-07-06 16:20:34 +0500

    new

--- android/app/src/main/kotlin/com/example/mebel_uz/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/mebel_uz/MainActivity.kt
@@ -1,5 +1,5 @@
-package com.example.mebel_uz
+package uz.dev.mebel_uz
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
